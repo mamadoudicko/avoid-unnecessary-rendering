969acfe161529352b16ef80239a67e34fb765aa4
Mamadou DICKO <mamadoudicko100@gmail.com>
2023-03-07 23:12:04 +0100

    📝 improve DX by adding comment

--- src/UserProfile.tsx
+++ src/UserProfile.tsx
@@ -9,7 +9,10 @@ export type User = {
   age: number;
 };
 
+// move to feat/memoization-example branch for memoized version
+// move to feat/use-ref-example branch for useRef version
 export const UserProfile = (): JSX.Element => {
+  // using useState will cause component re-render even if their props doesn't change
   const [user, setUser] = useState<User>({
     firstName: 'Mamadou',
     lastName: 'DICKO',
@@ -24,6 +27,7 @@ export const UserProfile = (): JSX.Element => {
       [event.target.name]: event.target.value,
     }));
   };
+
   const addUser = (event: React.SyntheticEvent): void => {
     event.preventDefault();
     console.log({ user });
